841fefd9765f0ac628d64a51cb1e3a66a8f5bd65
moritalous <moritalous@gmail.com>
2022-08-14 10:47:14 +0900

    Update

--- .remarkrc.js
+++ .remarkrc.js
@@ -59,7 +59,20 @@ const remarkConfig = {
             allowDangerousHtml: true
         }],
         rehypeSlug,
-        rehypeAutolinkHeadings
+        [rehypeAutolinkHeadings, {content: [
+            h('span.anchor', 
+                h('svg.octicon.octicon-link', {
+                    viewBox: '0 0 16 16', 
+                    version: '1.1', 
+                    width: '16',
+                    height: '16',
+                    'aria-hidden': 'true'
+                }, 
+                h('path', {
+                    'fill-rule': 'evenodd',
+                    'd': 'M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z'
+                })))
+          ]}]
     ]
 }
 
@@ -103,13 +116,6 @@ function myRemarkPlugin() {
                     data.hName = tagName
                     data.hProperties = h(tagName, { class: 'flash mt-3 flash-success' }).properties    
                 }
-                // if (node.name !== 'note') return
-
-                // const data = node.data || (node.data = {})
-                // const tagName = node.type === 'textDirective' ? 'span' : 'div'
-
-                // data.hName = tagName
-                // data.hProperties = h(tagName, { class: 'flash mt-3' }).properties
             }
         })
     }
