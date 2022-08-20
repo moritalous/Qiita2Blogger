8cd43deddf4df20a82636e242177a246112bf8dd
moritalous <moritalous@gmail.com>
2022-08-20 21:27:46 +0900

    目次の開閉を追加

--- .remarkrc.js
+++ .remarkrc.js
@@ -10,6 +10,7 @@ import rehypeHighlight from 'rehype-highlight'
 import rehypeStringify from 'rehype-stringify'
 import rehypeSlug from 'rehype-slug'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import remarkCollapse from 'remark-collapse'
 
 import cmake from 'highlight.js/lib/languages/cmake'
 import dart from 'highlight.js/lib/languages/dart'
@@ -40,7 +41,8 @@ const remarkConfig = {
         remarkBreaks,
         remarkFrontmatter,
         remarkGfm,
-        [remarkToc, {heading: '目次', tight: true}],
+        [remarkToc, { heading: '目次', tight: true }],
+        [remarkCollapse, {test: '目次', summary: '目次を開く'}],
         [remarkImgLinks, {
             absolutePath: "https://image.xxx.com/"
         }],
@@ -59,20 +61,22 @@ const remarkConfig = {
             allowDangerousHtml: true
         }],
         rehypeSlug,
-        [rehypeAutolinkHeadings, {content: [
-            h('span.anchor', 
-                h('svg.octicon.octicon-link', {
-                    viewBox: '0 0 16 16', 
-                    version: '1.1', 
-                    width: '16',
-                    height: '16',
-                    'aria-hidden': 'true'
-                }, 
-                h('path', {
-                    'fill-rule': 'evenodd',
-                    'd': 'M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z'
-                })))
-          ]}]
+        [rehypeAutolinkHeadings, {
+            content: [
+                h('span.anchor',
+                    h('svg.octicon.octicon-link', {
+                        viewBox: '0 0 16 16',
+                        version: '1.1',
+                        width: '16',
+                        height: '16',
+                        'aria-hidden': 'true'
+                    },
+                        h('path', {
+                            'fill-rule': 'evenodd',
+                            'd': 'M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z'
+                        })))
+            ]
+        }]
     ]
 }
 
@@ -88,33 +92,33 @@ function myRemarkPlugin() {
                 if (node.name == 'note') {
                     const data = node.data || (node.data = {})
                     const tagName = node.type === 'textDirective' ? 'span' : 'div'
-    
+
                     data.hName = tagName
-                    data.hProperties = h(tagName, { class: 'flash mt-3' }).properties    
+                    data.hProperties = h(tagName, { class: 'flash mt-3' }).properties
                 } else if (node.name == 'note-info') {
                     const data = node.data || (node.data = {})
                     const tagName = node.type === 'textDirective' ? 'span' : 'div'
-    
+
                     data.hName = tagName
-                    data.hProperties = h(tagName, { class: 'flash mt-3' }).properties    
+                    data.hProperties = h(tagName, { class: 'flash mt-3' }).properties
                 } else if (node.name == 'note-warn') {
                     const data = node.data || (node.data = {})
                     const tagName = node.type === 'textDirective' ? 'span' : 'div'
-    
+
                     data.hName = tagName
-                    data.hProperties = h(tagName, { class: 'flash mt-3 flash-warn' }).properties    
+                    data.hProperties = h(tagName, { class: 'flash mt-3 flash-warn' }).properties
                 } else if (node.name == 'note-alert') {
                     const data = node.data || (node.data = {})
                     const tagName = node.type === 'textDirective' ? 'span' : 'div'
-    
+
                     data.hName = tagName
-                    data.hProperties = h(tagName, { class: 'flash mt-3 flash-error' }).properties    
+                    data.hProperties = h(tagName, { class: 'flash mt-3 flash-error' }).properties
                 } else if (node.name == 'note-success') {
                     const data = node.data || (node.data = {})
                     const tagName = node.type === 'textDirective' ? 'span' : 'div'
-    
+
                     data.hName = tagName
-                    data.hProperties = h(tagName, { class: 'flash mt-3 flash-success' }).properties    
+                    data.hProperties = h(tagName, { class: 'flash mt-3 flash-success' }).properties
                 }
             }
         })
